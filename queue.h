199fc7c049eb7a7abb5e18789aa7814a14ec2dec
austinar59 <184719531+austinar59@users.noreply.github.com>
2025-02-28 11:27:38 -0600

    Update queue.h
    tabbing fix

--- queue.h
+++ queue.h
@@ -1,17 +1,17 @@
 #ifndef QUEUE_H
 #define QUEUE_H
- typedef struct QNode {
-   int val;
-   struct QNode* next;
- } QNode;
-	typedef struct Queue {
-		QNode* top;
-		int size;
-		QNode* end;
-	} Queue;
-	Queue* createQueue(int);
-	int enqueue(Queue*, int);
-	int dequeue(Queue*);
-	int peek(Queue*);	
+typedef struct QNode {
+	int val;
+	struct QNode* next;
+} QNode;
+typedef struct Queue {
+	QNode* top;
+	int size;
+	QNode* end;
+} Queue;
+Queue* createQueue(int);
+int enqueue(Queue*, int);
+int dequeue(Queue*);
+int peek(Queue*);	
  Queue* destroyQueue(Queue*);
 #endif
